93ae6a731bb5a9ec2887787277f51f7342ab3d07
To_Onoma_Sou <koutlisgreg@gmail.com>
2026-09-03 10:30:52 +0300

    Add task print methods and LINQ filtering

--- Program.cs
+++ Program.cs
@@ -1,69 +1,61 @@
 ﻿
-using System.Collections;
 using GsocPrep.Models;
 using GsocPrep.Models.Enums;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 public class Program
 {
     public static void Main()
     {
-        WorkTask task = new WorkTask();
+        WorkTask task = new WorkTask
         {
-            task.Id = 1;
-            task.Title = "Hello World";
-            task.Priority = Priority.High;
-            task.IsCompleted = true;
+            Id = 1,
+            Title = "Hello World",
+            Priority = Priority.High,
+            IsCompleted = true,
 
-            Console.WriteLine(task.Title);
-        }
+        };
+        
 
-        WorkTask task2 = new WorkTask();
+
+        WorkTask task2 = new WorkTask
         {
-            task2.Id = 2;
-            task2.Title = "Hello fuck u";
-            task2.Priority = Priority.Low;
-            task2.IsCompleted = true;
+            Id = 2,
+            Title = "Hello fuck u",
+            Priority = Priority.Low,
+            IsCompleted = false
 
-            Console.WriteLine(task2.Title);
-        }
+        };
 
-        WorkTask task3 = new WorkTask();
+        WorkTask task3 = new WorkTask
         {
-            task3.Id = 3;
-            task3.Title = "Hello World";
-            task3.Priority = Priority.Low;
-            task3.IsCompleted = true;
+            Id = 3,
+            Title = "Hello World",
+            Priority = Priority.Low,
+            IsCompleted = true,
+        };
 
-            Console.WriteLine(task3.Title);
 
-
-        }
-        WorkTask task4 = new WorkTask();
+        WorkTask task4 = new WorkTask
         {
-            task4.Id = 4;
-            task4.Title = "Hello World";
-            task4.Priority = Priority.Low;
-            task4.IsCompleted = true;
-
-            Console.WriteLine(task4.Title);
+            Id = 4,
+            Title = "Hello World",
+            Priority = Priority.Low,
+            IsCompleted = false
 
 
-        }
-        WorkTask task5 = new WorkTask();
+        };
+        WorkTask task5 = new WorkTask
         {
-            task5.Id = 5;
-            task5.Title = "Hello World";
-            task5.Priority = Priority.Low;
-            task5.IsCompleted = true;
-
-            Console.WriteLine(task5.Title);
+            Id = 5,
+            Title = "Hello World",
+            Priority = Priority.Low,
+            IsCompleted = true,
 
 
-        }
+        };
 
 
-        List<WorkTask> workTasks = new List<WorkTask>()
+        List<WorkTask> workTasks = new List<WorkTask>
         {
             task, task2, task3, task4, task5
         };
@@ -83,13 +75,58 @@ public class Program
 
     public void CompleteTask(WorkTask task)
     {
-        if (task.IsCompleted == true)
+        task.IsCompleted = true;
+    }
+
+    public void PrintTask(WorkTask task)
+    {
+        Console.WriteLine(task.Id);
+        Console.WriteLine(task.Title);
+        Console.WriteLine(task.Priority);
+
+        if (task.IsCompleted)
         {
-            task.IsCompleted = false;
+            Console.WriteLine("completed");
         }
         else
         {
-            task.IsCompleted = true;
+            Console.WriteLine("not completed");
         }
     }
+
+    public void PrintTasks(List<WorkTask> tasks)
+    {
+        foreach (var task in tasks)
+        {
+            PrintTask(task);
+        }
+
+        
+    }
+    
+    public List<WorkTask> GetInCompleteTasks(List<WorkTask> tasks)
+    {
+        List<WorkTask> incompleteTasks = new List<WorkTask>();
+
+        foreach (var task in tasks)
+        {
+            if (task.IsCompleted == false)
+            {
+                incompleteTasks.Add(task);
+            }
+        }
+
+        return incompleteTasks;
+    }
+    //LINQ
+    //public List<WorkTask> GetInCompletetasks(List<WorkTask> tasks){
+    //return tasks
+    //  .Where(task=> !task.IsCompleted)
+    //  .ToList();
+    // }
+        
 }
+
+    
+
+    
